bfd3933a347d9ac9664aa1e1cd72dad3074b2217
ethan <hertlethan@gmail.com>
2023-08-27 19:41:36 -0400

    added literally two comments, i SWEAR i'm done now

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -459,6 +459,7 @@ function createEvent()
   renderEventPlanner(firstDay); //updates event calendar
 }
 
+//function to delete events from the calendar
 function deleteEvent()
 {
   elementClicked = event.target //retrieves element that was clicked
@@ -695,6 +696,7 @@ var clearCityInput = document.getElementById('clear-city');
 var weatherContainer = document.getElementById('weather-container');
 var city;
 
+//if there is no local storage variable set for the saved city, create an empty one
 if (!localStorage.getItem('savedCity'))
 {
   localStorage.setItem('savedCity', '');
